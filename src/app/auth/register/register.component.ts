48611c1f0ece63d63a393233c1d1b73ac808aa18
Ricardo Terán <39249155+Rikardo816@users.noreply.github.com>
2024-05-09 21:28:56 -0500

    FEAT: Validar contraseñas iguales

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  public formSubmitted = false;
 
+  public registerForm = this.fb.group(
+    {
+      name: ['Ricardo', [Validators.required, Validators.minLength(3)]],
+      email: ['ricardo@gmail.com', [Validators.required, Validators.email]],
+      password: ['123456', [Validators.required, Validators.minLength(6)]],
+      password2: ['123456', [Validators.required, Validators.minLength(6)]],
+      terminos: [true, Validators.requiredTrue],
+    },
+    {
+      validators: this.passwordsIguales('password', 'password2')
+    } 
+  );
+
+  constructor(private fb: FormBuilder) {}
+
+  crearUsuario() {
+    this.formSubmitted = true;
+    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      console.log('Posteando formulario');
+    } else {
+      console.log('Formulario no es correcto...');
+    }
+  }
+
+  campoNoValido(campo: string): boolean {
+    if (this.registerForm.get(campo)?.invalid && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  aceptaTerminos() {
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
+  }
+  passwordInvalid() {
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+    if (pass1 !== pass2 && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  passwordsIguales(pass1Name: string, pass2Name: string) {
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if (pass1Control?.value === pass2Control?.value) {
+        pass2Control?.setErrors(null);
+      } else {
+        pass2Control?.setErrors({noEsIgual: true})
+      }
+    }
+  }
 }
